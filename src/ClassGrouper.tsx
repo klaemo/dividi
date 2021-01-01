9ca6aba30407c57407e39d438d5cd6cd0884f6aa
Clemens Stolle <klaemo@fastmail.fm>
2021-01-01 14:52:34 +0100

    various UI changes

--- src/ClassGrouper.tsx
+++ src/ClassGrouper.tsx
@@ -7,16 +7,27 @@ import {
   FennelResult,
 } from "./fennel";
 
-function Result({
-  result,
-}: {
-  result: FennelResult;
-}) {
+/** Experimental highlight colors for the partitions */
+const PARTITION_COLORS = [
+  "text-green-500",
+  "text-blue-500",
+  "text-pink-500",
+  "text-yellow-500",
+  "text-purple-500",
+];
+
+function getPartitionIndex(result: FennelResult, id: number) {
+  return result.partions.findIndex((partition) => partition.includes(id));
+}
+
+function Result({ result }: { result: FennelResult }) {
   return (
     <>
       {result.partions.map((group, index) => (
         <>
-          <div class="mt-3 font-bold">Gruppe {index + 1}</div>
+          <div class={`mt-3 font-bold ${PARTITION_COLORS[index]}`}>
+            Gruppe {index + 1}
+          </div>
           <div>{group.join(", ")}</div>
         </>
       ))}
@@ -24,25 +35,66 @@ function Result({
   );
 }
 
-function Student({ id, contacts }: { id: number; contacts: number[] }) {
+function Student({
+  id,
+  contacts,
+  onChange,
+  result,
+}: {
+  id: number;
+  contacts: number[];
+  onChange: (contacts: number[]) => void;
+  result: FennelResult | null;
+}) {
+  const inputClasses =
+    "w-12 mr-2 border rounded border-gray-200 px-1 py-1 text-center focus:outline-none focus:ring focus:border-indigo-300";
+  const color =
+    result !== null ? PARTITION_COLORS[getPartitionIndex(result, id)] : "";
+
   return (
     <tr>
-      <td class="px-6 py-4 whitespace-nowrap">
-        {/* <input class="w-12" type="number" min="0" required value={id} /> */}
+      <td
+        class={`px-6 py-2 whitespace-nowrap ${color}`}
+        aria-label="Schüler ID"
+      >
         {id}
       </td>
-      <td class="px-6 py-4 whitespace-nowrap">
+      <td class="px-6 py-2 whitespace-nowrap">
         {contacts.map((contact, index) => (
           <input
-            class="w-12"
+            class={`${inputClasses} ${
+              result ? PARTITION_COLORS[getPartitionIndex(result, contact)] : ""
+            }`}
             key={index}
-            type="number"
+            inputMode="numeric"
+            pattern="\d*"
             min="0"
-            required
             value={contact}
+            onChange={(event) => {
+              if (event.target instanceof HTMLInputElement) {
+                const newContacts = [...contacts];
+                newContacts[index] = parseInt(event.target.value);
+                onChange(newContacts);
+              }
+            }}
           />
         ))}
-        <input class="w-12" key="next" type="number" min="0" />
+        <input
+          class={inputClasses}
+          key="next"
+          inputMode="numeric"
+          pattern="\d*"
+          min="0"
+          onBlur={(event) => {
+            if (event.target instanceof HTMLInputElement) {
+              const value = parseInt(event.target.value);
+              if (Number.isInteger(value)) {
+                onChange([...contacts, value]);
+                event.target.value = "";
+              }
+            }
+          }}
+        />
       </td>
     </tr>
   );
@@ -51,10 +103,12 @@ function Student({ id, contacts }: { id: number; contacts: number[] }) {
 const initialData = [{ id: 1, contacts: [] }];
 
 function ClassGrouper({
+  result,
   setResult,
   data,
   setData,
 }: {
+  result: FennelResult | null;
   setResult: (result: FennelResult | null) => void;
   data: StudentInformation[];
   setData: (data: StudentInformation[]) => void;
@@ -81,37 +135,7 @@ function ClassGrouper({
         setResult(result);
       }}
     >
-      <div class="shadow overflow-hidden border-b border-gray-200 rounded-md mt-5">
-        <table class="table-fixed min-w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                class="w-1/6 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Schüler
-              </th>
-              <th
-                scope="col"
-                class="w-5/6 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Kontakte
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-            {data.map((student, index) => (
-              <Student
-                key={index}
-                id={student.id}
-                contacts={student.contacts}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <div class="flex justify-between items-center mt-3">
+      <div class="sticky top-0 flex justify-between items-center p-3 bg-white">
         <div>
           <label>
             <span class="pr-2">Gruppen:</span>
@@ -120,16 +144,18 @@ function ClassGrouper({
               type="number"
               value={numberOfGroups}
               min={2}
-              onChange={(event) =>
-                setNumberOfGroups(parseInt(event.target.value))
-              }
+              onChange={(event) => {
+                if (event.target instanceof HTMLInputElement) {
+                  setNumberOfGroups(parseInt(event.target.value));
+                }
+              }}
             ></input>
           </label>
         </div>
         <div class="flex">
           <button
             type="button"
-            class="mr-8"
+            class="mr-8 hover:text-gray-500"
             onClick={() => setData(exampleClass)}
           >
             Beispieldaten laden
@@ -153,6 +179,45 @@ function ClassGrouper({
           </button>
         </div>
       </div>
+
+      <div class="shadow overflow-hidden border-b border-gray-200 rounded-md mt-2">
+        <table class="table-fixed min-w-full divide-y divide-gray-200">
+          <thead class="bg-gray-50">
+            <tr>
+              <th
+                scope="col"
+                class="w-1/6 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Schüler
+              </th>
+              <th
+                scope="col"
+                class="w-5/6 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Kontakte
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+            {data.map((student, index) => (
+              <Student
+                key={index}
+                id={student.id}
+                result={result}
+                contacts={student.contacts}
+                onChange={(contacts) => {
+                  const newData = [...data];
+                  newData[index] = {
+                    ...newData[index],
+                    contacts,
+                  };
+                  setData(newData);
+                }}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </form>
   );
 }
@@ -163,10 +228,13 @@ export function ClassGrouperApp() {
 
   return (
     <>
-      <ClassGrouper data={data} setData={setData} setResult={setResult} />
-      {result !== null && (
-        <Result result={result} />
-      )}
+      <ClassGrouper
+        data={data}
+        setData={setData}
+        setResult={setResult}
+        result={result}
+      />
+      {result !== null && <Result result={result} />}
     </>
   );
 }
