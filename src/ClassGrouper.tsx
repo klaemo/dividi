cf20435e0798f2e2efea1678dd2a8a6affe1df31
Clemens Stolle <klaemo@fastmail.fm>
2021-01-07 14:57:41 +0100

    last one

--- src/ClassGrouper.tsx
+++ src/ClassGrouper.tsx
@@ -64,7 +64,7 @@ export function ClassGrouperApp() {
         }
       }}
     >
-      <div class="sticky top-0 flex justify-end items-center p-4 -mx-4 ">
+      <div class="sticky top-0 flex justify-end items-center bg-white p-4 -mx-4 ">
         <div class="flex">
           {data !== initialData && (
             <button
